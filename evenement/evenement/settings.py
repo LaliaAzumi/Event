f67f10367a3bdcb1d87e6571048104a8ce4db9fc
LaliaAzumi <laliaazumii@gmail.com>
2024-12-04 09:59:00 +0200

    creation

--- evenement/evenement/settings.py
+++ evenement/evenement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event',
 ]
 
 MIDDLEWARE = [
@@ -73,14 +74,16 @@ WSGI_APPLICATION = 'evenement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'evenement',
+        'USER' : 'root',
+        'PASSWORD': '',
+        'PORT' :'3306'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
